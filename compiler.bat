cddeecc62b3e282f0cf2edf4992af7743e90dcce
Ink <andryvorobey123@gmail.com>
2022-05-13 17:40:57 +0300

    Update

---
+++ compiler.bat
@@ -0,0 +1,13 @@
+@echo on & set /P file="File name for compilation (for example test.py): " & set /P compression="Compress the file? (y/n): " & set /P console="Disable console? (y/n): "
+
+pip install virtualenv & virtualenv venv & call venv\Scripts\activate
+
+pip install -r requirements.txt & if "%compression%" == "y" (pip install zstandard==0.17.0) else (echo Y|pip uninstall zstandard)
+
+if "%console%" == "y" (
+    nuitka --onefile --windows-disable-console %file%
+) else (
+    nuitka --onefile %file%
+)
+
+pause
