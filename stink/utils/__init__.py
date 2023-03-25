2081243e8767ad8c2e0bc74a58bdc457d07295a5
Ink <andryvorobey123@gmail.com>
2023-03-25 15:40:16 +0200

    Added custom AES implementation

--- stink/utils/__init__.py
+++ stink/utils/__init__.py
@@ -1,5 +1,6 @@
 from .autostart import Autostart
 from .message import Message
 from .senders import TelegramSender, ServerSender, DiscordSender
+from .pyaes import AESModeOfOperationGCM
 
-__all__ = ["Autostart", "Message", "TelegramSender", "ServerSender", "DiscordSender"]
+__all__ = ["Autostart", "Message", "TelegramSender", "ServerSender", "DiscordSender", "AESModeOfOperationGCM"]
