4a5386dd353a9193db9349c0d32e3df0a6642b59
FallenAstaroth <andryvorobey123@gmail.com>
2023-04-06 22:14:36 +0300

    File renamed

--- stink/helpers/__init__.py
+++ stink/helpers/__init__.py
@@ -1,6 +1,27 @@
 from . import config
 from . import functions
 from .multipart import MultipartFormDataEncoder
-from .structures import DataBlob, ProcessEntry32, ProcessMemoryCountersEx, DisplayDevice, MemoryStatusEx, UlargeInteger
+from .structures import (
+    DataBlob,
+    ProcessEntry32,
+    ProcessMemoryCountersEx,
+    DisplayDevice,
+    MemoryStatusEx,
+    UlargeInteger,
+    BitmapInfoHeader,
+    BitmapInfo
+)
 
-__all__ = ["MultipartFormDataEncoder", "config", "functions", "DataBlob", "ProcessEntry32", "ProcessMemoryCountersEx", "DisplayDevice", "MemoryStatusEx", "UlargeInteger"]
+__all__ = [
+    "MultipartFormDataEncoder",
+    "config",
+    "functions",
+    "DataBlob",
+    "ProcessEntry32",
+    "ProcessMemoryCountersEx",
+    "DisplayDevice",
+    "MemoryStatusEx",
+    "UlargeInteger",
+    "BitmapInfoHeader",
+    "BitmapInfo"
+]
